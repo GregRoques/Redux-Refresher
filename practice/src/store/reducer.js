9d80488d28eeb4f142489ee700639f22d6daaad2
Greg Roques <greg.roques@gmail.com>
2019-05-19 10:34:44 -0400

    hi

--- practice/src/store/reducer.js
+++ practice/src/store/reducer.js
@@ -8,9 +8,9 @@ const reducer = ( state = initialState, action ) => {
     switch ( action.type ) {
         case actionTypes.ADD_PERSON:
             const newPerson = {
-                id: Math.random(), // not really unique but good enough here!
-                name: 'Max',
-                age: Math.floor( Math.random() * 40 )
+                id: Math.random(),
+                name: action.personData.name,
+                age: action.personData.age
             }
             return {
                 ...state,
