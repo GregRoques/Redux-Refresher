0372a2df4cb38d132e484c397e85614ce6ee7374
Greg Roques <greg.roques@gmail.com>
2019-05-21 16:22:20 -0400

    firebase packages

--- marioplan/src/index.js
+++ marioplan/src/index.js
@@ -5,13 +5,21 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 // Redux things
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
 import rootReducer from './store/reducers/rootReducer'
+import { getFirestore, reduxFirestore } from 'redux-firestore'
+import { getFirebase, reactReduxFirebase } from 'react-redux-firebase'
 
-
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(rootReducer, 
+    compose(
+    applyMiddleware(thunk.withExtraArgument({getFirebase, getFirestore})),
+    reduxFirestore(),
+    reactReduxFirebase()
+    
+    )
+);
 
 ReactDOM.render(
 <Provider store={store}>
