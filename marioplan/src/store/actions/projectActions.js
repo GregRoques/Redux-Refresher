047a2abdeee7c5b95bb2895f60fe76ceec2146a0
Greg Roques <greg.roques@gmail.com>
2019-05-22 16:37:35 -0400

    combined projects

--- marioplan/src/store/actions/projectActions.js
+++
@@ -1,7 +0,0 @@
-export const createProject = (project) =>{
-    return(dispatch, getState,{getFirebase, getFirestore}) =>{
-        //make async call
-        dispatch({ type:'CREATE_PROJECT', project});
-
-    }
-}
